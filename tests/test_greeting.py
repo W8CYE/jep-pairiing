ae48883c8ec6e070afd7005fcbfc0c2610d37e87
Josh Peterson <ccie7207@gmail.com>
2024-02-29 20:10:48 -0500

    added test for hello jim

--- tests/test_greeting.py
+++ tests/test_greeting.py
@@ -1,2 +1,7 @@
+from greeting import greeting
+
 def test_greeting_given_josh_returns_hello_josh():
     assert greeting('Josh') == 'Hello Josh!'
+
+def test_greeting_given_josh_returns_hello_jim():
+    assert greeting('Josh') == 'Hello Jim!'
