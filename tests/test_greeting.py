a94522105dd5fc8f7c05a479c81179114b7cb619
James Prior <jep200404@columbus.rr.com>
2024-02-29 20:16:06 -0500

    Fix test_greeting_given_jim_returns_hello_jim.
    Co-authored-by: James Prior <jep200404@columbus.rr.com>

--- tests/test_greeting.py
+++ tests/test_greeting.py
@@ -3,5 +3,5 @@ from greeting import greeting
 def test_greeting_given_josh_returns_hello_josh():
     assert greeting('Josh') == 'Hello Josh!'
 
-def test_greeting_given_josh_returns_hello_jim():
-    assert greeting('Josh') == 'Hello Jim!'
+def test_greeting_given_jim_returns_hello_jim():
+    assert greeting('Jim') == 'Hello Jim!'
