3dcd3cf04c2a216793bea6788cac5cf74582a29c
Josh Peterson <ccie7207@gmail.com>
2024-02-29 20:23:46 -0500

    implemented test for hello world

--- tests/test_greeting.py
+++ tests/test_greeting.py
@@ -1,7 +1,13 @@
 from greeting import greeting
 
+
 def test_greeting_given_josh_returns_hello_josh():
-    assert greeting('Josh') == 'Hello Josh!'
+    assert greeting("Josh") == "Hello Josh!"
+
 
 def test_greeting_given_jim_returns_hello_jim():
-    assert greeting('Jim') == 'Hello Jim!'
+    assert greeting("Jim") == "Hello Jim!"
+
+
+def test_greeting_given_world_returns_hello_world():
+    assert greeting("World") == "Hello World!"
